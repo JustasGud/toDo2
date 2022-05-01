95ab0b92186ba47dc57e04b14c6a49f83f4f6be4
Justas Gudonis <gudonisjustas@gmail.com>
2022-05-01 20:28:11 +0300

    to do list is saved in database

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,13 +9,43 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Wake up",
+});
+const item2 = new Item({
+  name: "Drink coffe",
+});
+const item3 = new Item({
+  name: "Exercise",
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get("/", function (req, res) {
-  const day = date.getDate();
-
-  res.render("list", { listTitle: day, newListItems: items });
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Succesfully saved!");
+        }
+      });
+      res.redirect("/");
+    } else if (err) {
+      console.log(err);
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
 });
 
 app.post("/", function (req, res) {
